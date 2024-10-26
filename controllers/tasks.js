209b320a524c16f9e6568cea91049217cc2416c2
lavish <lavish758@gmail.com>
2024-10-26 08:58:18 +0530

    updates Route

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -43,7 +43,10 @@ exports.getAllTasks=async (req,res)=>{
         //get id of the user from header
         const {id}=req.headers;
 
-        const userData=await User.findById(id);
+        const userData=await User.findById(id).populate({
+            path:"tasks",
+            options:{sort:{createdAt:-1}}
+        });
         
         res.status(200).json({
             data:userData
@@ -56,5 +59,142 @@ exports.getAllTasks=async (req,res)=>{
             message:"Internal server error"
         });
         
+    }
+}
+
+//delete Tasks
+
+exports.deleteTask=async (req,res)=>{
+    try{
+        const {id}=req.params;
+        console.log("Taksid",id);
+        await TASK.findByIdAndDelete(id);
+        console.log("I am Inside delete taks")
+        const userId=req.headers.id;
+        console.log("userid",userId);
+        await User.findByIdAndUpdate(userId,{$pull:{tasks:id}});
+
+        res.status(200).json({
+            message:"task deleted successfully"
+        })
+
+    }
+    catch(error){
+        console.log(error);
+        res.status(400).json({
+            message:"Internal Server Error in Deletion"
+        })
+
+    }
+}
+
+exports.updateTask=async (req,res)=>{
+    try{
+        const {id}=req.params;
+        const {title,desc}=req.body;
+        await TASK.findByIdAndUpdate(id,{title:title,desc:desc});
+  
+
+        res.status(200).json({
+            message:"Task updated successfully"
+        })
+
+    }
+    catch(error){
+        console.log(error);
+        res.status(400).json({
+            message:"Task is unable to update successfully"
+        })
+
+    }}
+
+ exports.updateCompleteTask=async (req,res)=>{
+        try{
+            const {id}=req.params;
+            console.log(id);
+            const TaskData=await TASK.findById(id);
+            const completeTask=TaskData.complete;
+            console.log("completeTask",completeTask);
+            await TASK.findByIdAndUpdate(id,{complete:!completeTask})
+      
+    
+            res.status(200).json({
+                message:"complete updated successfully"
+            })
+    
+        }
+        catch(error){
+            console.log(error);
+            res.status(400).json({
+                message:"Internal Server Error to mark complete task"
+            })
+    
+        }}
+
+ exports.updateImportantTask=async (req,res)=>{
+            try{
+                const {id}=req.params;
+                console.log(id);
+                const TaskData=await TASK.findById(id);
+                const importantTask=TaskData.important;
+                console.log("importantTask",importantTask);
+                await TASK.findByIdAndUpdate(id,{important:!importantTask})
+          
+        
+                res.status(200).json({
+                    message:"important updated successfully"
+                })
+        
+            }
+            catch(error){
+                console.log(error);
+                res.status(400).json({
+                    message:"Internal Server Error to mark complete task"
+                })
+        
+            }}
+    
+
+exports.getImpTasks=async (req,res)=>{
+    try{
+        const {id}=req.headers;
+        const Data=await User.findById(id).populate({
+            path:"tasks",
+            match:{important:true},
+            options:{sort:{createdAt:-1}},
+
+        })
+        const impTaskData=Data.tasks;
+        res.status(200).json({
+            data:impTaskData
+        })
+
+    }catch(error){
+        console.log(error);
+        res.status(400).json({
+            message:"Internal server error",
+        })
+    }
+}
+
+exports.getCmpTasks=async (req,res)=>{
+    try{
+        const {id}=req.headers;
+        const Data=await User.findById(id).populate({
+            path:"tasks",
+            match:{complete:true},
+            options:{sort:{createdAt:-1}},
+
+        })
+        const cmpTaskData=Data.tasks;
+        res.status(200).json({
+            data:cmpTaskData
+        })
+
+    }catch(error){
+        console.log(error);
+        res.status(400).json({
+            message:"Internal server error",
+        })
     }
 }
--- routes/routes.js
+++ routes/routes.js
@@ -6,11 +6,18 @@ const {auth}=require('./authMiddleware.js')
 //write controllers
 const {signup}=require('../controllers/auth.js');
 const {login}=require('../controllers/auth.js');
-const {task, getAllTasks}=require('../controllers/tasks.js');
+const {task, getAllTasks,deleteTask,updateTask,updateImportantTask,updateCompleteTask,getImpTasks,getCmpTasks}=require('../controllers/tasks.js');
 //
 Router.post('/signup',signup);
 Router.post('/login',login);
 Router.post('/create-task',auth,task);
-Router.post('/getAllTasks',auth,getAllTasks);
+Router.get('/getAllTasks',auth,getAllTasks);
+Router.delete('/delete-Task/:id',auth,deleteTask);
+Router.put('/update-task/:id',auth,updateTask);
+Router.put('/update-imp-task/:id',auth,updateImportantTask);
+Router.put('/update-cmp-task/:id',auth,updateCompleteTask);
+Router.get('/getImpTasks',auth,getImpTasks);
+Router.get('/getCmpTasks',auth,getImpTasks);
+
 
 module.exports=Router;
